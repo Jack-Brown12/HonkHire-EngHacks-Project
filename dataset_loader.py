4b2ccf1aecd6c7839352a8ad6d8366dd12135a12
NoahBradwin <noahbradwin@gmail.com>
2026-03-07 15:26:19 -0500

    finished skills frequency caregories

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,57 +1,44 @@
 import kagglehub
 import pandas as pd
-from skill_extraction import extract_skills
 
 # Download latest version
-path = kagglehub.dataset_download("ravindrasinghrana/job-description-dataset")
+path = kagglehub.dataset_download("asaniczka/software-engineer-job-postings-linkedin")
 
 print("Path to dataset files:", path)
 
 # Load the dataset into a pandas DataFrame
-df = pd.read_csv(f"{path}/job_descriptions.csv")
+df = pd.read_csv(f"{path}/postings.csv")
 print("Successfully loaded dataset into DataFrame")
 
-# Count the frequency of each skill in the dataset using extract_skills function
+# Count the frequency of each skill in the dataset
 skill_frequency = {}
-for skill in df["skills"]:
-        skill_tuple = extract_skills(skill)
-        for skill_confidence in skill_tuple:
-            skill = skill_confidence[0]
-            if skill in skill_frequency:
-                skill_frequency[skill] += 1
-            else:
-                skill_frequency[skill] = 1
+for skills in df["job_skills"]:
+    for skill in str(skills).split(","):
+        skill = skill.strip().lower()
+        if skill in skill_frequency:
+            skill_frequency[skill] += 1
+        else:
+            skill_frequency[skill] = 1
 print("Successfully created skill frequency dictionary.")
 
-# Separate skills into three categories: common, uncommon, and rare.
-sorted_skills = sorted(skill_frequency.items(), key=lambda x: x[1], reverse=True)
-common_skills = {}
-uncommon_skills = {}
-rare_skills = {}
-third = len(skill_frequency) // 3
 
-# Change frequency into percent occurrence using frequency/total frequency * 100
-total_frequency = sum(skill_frequency.values())
-
-for i, (skill, frequency) in enumerate(sorted_skills):
-    if i < third:
-        common_skills[skill] = frequency/total_frequency * 100
-    elif i < 2 * third:
-        uncommon_skills[skill] = frequency/total_frequency * 100
-    else:
-        rare_skills[skill] = frequency/total_frequency * 100
+# Change frequency into percent occurrence using frequency/num postings * 100
+total_postings = len(df)
+common_skills = [(skill, frequency) for skill, frequency in skill_frequency.items() if frequency / total_postings * 100 >= 10]
+uncommon_skills = [(skill, frequency) for skill, frequency in skill_frequency.items() if frequency / total_postings * 100 >= 5 and frequency / total_postings * 100 < 10]
+rare_skills = [(skill, frequency) for skill, frequency in skill_frequency.items() if frequency / total_postings * 100 > 0.5 and frequency / total_postings * 100 < 5]
 
 print("Successfully categorized skills into common, uncommon, and rare.")
 
 # Write skills to separate text files with percent occurrence.
 with open("common_skills.txt", "w") as outfile:
-    for skill in common_skills:
-        outfile.write(f"{skill}: {common_skills[skill]:.2f}%\n")
+    for skill, frequency in common_skills:
+        outfile.write(f"{skill}: {frequency / total_postings * 100:.2f}%\n")
 with open("uncommon_skills.txt", "w") as outfile:
-    for skill in uncommon_skills:
-        outfile.write(f"{skill}: {uncommon_skills[skill]:.2f}%\n")
+    for skill, frequency in uncommon_skills:
+        outfile.write(f"{skill}: {frequency / total_postings * 100:.2f}%\n")
 with open("rare_skills.txt", "w") as outfile:
-    for skill in rare_skills:
-        outfile.write(f"{skill}: {rare_skills[skill]:.2f}%\n")
+    for skill, frequency in rare_skills:
+        outfile.write(f"{skill}: {frequency / total_postings * 100:.2f}%\n")
 
 print("Successfully wrote skills to separate text files.")
